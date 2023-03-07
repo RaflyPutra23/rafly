33de8b54021e85975aab598368b6ac3f7b2cca3b
RaflyPutra23 <127195822+RaflyPutra23@users.noreply.github.com>
2023-03-07 16:43:54 +0700

    Add files via upload

---
+++ Lunch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB D B-20
+ */
+class Lunch {
+
+    static void setKonsumsi(Makanan nasi, Minuman air) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
